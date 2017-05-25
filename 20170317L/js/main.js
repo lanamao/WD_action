8ff2226bf878eaf31ae6e6a9a83999c375bf5f90
lanamao <lisa811114@gmail.com>
2017-05-25 13:52:00 +0800

    2017/03~2017/04
    works

---
+++ 20170317L/js/main.js
@@ -0,0 +1,14 @@
+﻿// 禮包效果 --------
+$(function($){
+		$(".ftoollist li").mouseover(function(){
+			$(this).siblings().removeClass("on");
+			$(this).addClass("on");
+			var preNumber=$(this).prevAll().size();
+			$(".fimglist li").removeClass("onpre");
+			$(".fimglist li:nth-child("+preNumber+")").addClass("onpre");
+			var margin = 730;
+			margin = margin *preNumber; 
+			margin = margin * -1;
+			$(".fimglist").stop().animate({marginLeft: margin + "px"}, {duration: 500});
+		});
+	});
---
+++ 20170410L/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+
+//JS錨點
+//var url = document.location.href;//宣告網址字串
+//if(url.indexOf("?inframe=") >= 0 ) { //判斷url中是否包#nav字符串，如果包含将移動到.nav的位置
+//		$('html,body').animate({scrollTop:$('#nav').offset().top}, 200); 
+//}
+
+});
